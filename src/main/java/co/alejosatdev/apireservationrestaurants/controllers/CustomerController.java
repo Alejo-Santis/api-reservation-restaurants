6b08e303ae5e49f00064aab8ac61401494b169bb
Alejandro Santis Castro <alejosantismk11@gmail.com>
2023-07-16 13:07:35 -0500

    Agregando los controllers para los customers y restauranttable

--- src/main/java/co/alejosatdev/apireservationrestaurants/controllers/CustomerController.java
+++ src/main/java/co/alejosatdev/apireservationrestaurants/controllers/CustomerController.java
@@ -1,4 +1,45 @@
 package co.alejosatdev.apireservationrestaurants.controllers;
 
+import co.alejosatdev.apireservationrestaurants.entities.Customer;
+import co.alejosatdev.apireservationrestaurants.services.CustomerService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/customer")
 public class CustomerController {
+    @Autowired
+    private CustomerService customerService;
+
+    @GetMapping
+    public ResponseEntity<List<Customer>> getAllCustomers() {
+        List<Customer> customers = customerService.findAll();
+        return ResponseEntity.ok(customers);
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<Customer> getCustomer(@PathVariable Long id) {
+        Customer customer = customerService.findById(id);
+        return ResponseEntity.ok(customer);
+    }
+    @PostMapping
+    public ResponseEntity<Customer> saveCustomer(@Valid @RequestBody Customer customer) {
+        Customer newCustomer = customerService.save(customer);
+        return ResponseEntity.status(HttpStatus.CREATED).body(newCustomer);
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<Customer> updateCustomer(@PathVariable Long id,
+                                                   @Valid @RequestBody Customer customer) {
+        Customer updateCustomer = customerService.update(id, customer);
+        return ResponseEntity.ok(updateCustomer);
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteCustomer(@PathVariable Long id) {
+        customerService.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
 }
